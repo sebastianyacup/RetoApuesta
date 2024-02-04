fc3baaf1463a6e8a5975551c4f2f6650d1f0cf70
seagui <seagui@bancolombia.com.co>
2024-02-04 06:18:31 -0500

    microservicio ajuste

---
+++ RetoApuestas/src/test/java/com/apuestas/RetoApuestas/RetoApuestasApplicationTest.java
@@ -0,0 +1,12 @@
+package com.apuestas.RetoApuestas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RetoApuestasApplicationTest {
+
+	@Test
+	void main() {
+	}
+}
--- RetoApuestas/src/test/java/com/apuestas/RetoApuestas/RetoApuestasApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.apuestas.RetoApuestas;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RetoApuestasApplicationTests {
-
-}
